824df77d90343c1acad9d0de09ceb2c4925eaec8
KevinZ7 <kevin.z.zhou@gmail.com>
2018-09-27 05:25:59 +0000

    added social.js with the 1/7 functions

---
+++ social.js
@@ -0,0 +1,63 @@
+var data = {
+  f01: {
+    name: "Alice",
+    age: 15,
+    follows: ["f02", "f03", "f04"]
+  },
+  f02: {
+    name: "Bob",
+    age: 20,
+    follows: ["f05", "f06"]
+  },
+  f03: {
+    name: "Charlie",
+    age: 35,
+    follows: ["f01", "f04", "f06"]
+  },
+  f04: {
+    name: "Debbie",
+    age: 40,
+    follows: ["f01", "f02", "f03", "f05", "f06"]
+  },
+  f05: {
+    name: "Elizabeth",
+    age: 45,
+    follows: ["f04"]
+  },
+  f06: {
+    name: "Finn",
+    age: 25,
+    follows: ["f05"]
+  }
+};
+
+
+
+
+function followChat(){
+  var object = {};
+  var keyArray = Object.keys(data);
+  var counter = 0;
+
+  console.log(keyArray);
+  console.log(data.length);
+
+  for(var key in data){
+    object[keyArray[counter]] = {
+      name: data[key].name,
+      following: data[key].follows.length,
+      followers: 0
+    }
+
+    for(key2 in data){
+      if(data[key2].follows.includes(keyArray[counter])){
+        object[keyArray[counter]].followers += 1;
+      }
+    }
+
+  counter ++;
+  }
+  return object;
+}
+
+console.log(followChat());
